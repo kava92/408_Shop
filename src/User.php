844402fb226bed4b3b5c04ecfeca9aacf6e75a61
Zachariasz <zah3@o2.pl>
2016-06-10 16:12:14 +0200

    added User-class

--- src/User.php
+++ src/User.php
@@ -1,8 +1,103 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class User{
+    
+    public static function getUserById(mysqli $conn, $id){
+        $sql = "SELECT * FROM User WHERE id = $id";
+        $result = $conn->query($sql);
+        if($result->num_rows == 1){
+            return $result->fetch_assoc();
+        }else{
+            return false;
+        }
+    }
+     public static function login(mysqli $conn, $email, $password) {
+        $sql = "SELECT * FROM User WHERE email = '$email' ";
+        $result = $conn->query($sql);
+        if($result->num_rows == 1 ) {
+            $rowUser = $result->fetch_assoc();
+            if (password_verify($password, $rowUser['password'])){
+                return $rowUser['id'];
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+    
+    private $id;
+    private $name;
+    private $surname;
+    private $email;
+    private $password;
+    
+    
+    
+    public function __construct() {
+        $this->id = -1;
+        $this->name = 0;
+        $this->surname =0;
+        $this->email = '';
+        $this->password = '';
+        
+        
+    }
+    
+    public function getId(){
+        return $this->id;
+    }
+    
+    
+    public function setName($name){
+        $this->name = is_string($name) ? trim($name) : $this->name;
+    }
+    
+    public function getName(){
+        return $this->name;
+    }
+    
+    public function setSurname($surname){
+        $this->surname = is_string($surname) ? trim($surname) : $this->surname;
+    }
+    
+    public function getSurname(){
+        return $this->nsurname;
+    }
+    public function setEmail($email){
+        $this->email = is_string($email) ? trim($email) : $this->email;
+    }
+    
+    public function getEmail (){
+        return $this->email;
+    }
+    
+    public function setPassword($password, $retypePassword){
+        if($password != $retypePassword) {
+            return false;
+        }
+        $this->password = password_hash($password, PASSWORD_BCRYPT);
+        return true;
+    }
+    
+    public function saveToDB(mysqli $conn){
+        if ($this->id == -1){
+            $sql="INSERT INTO User (name, surname, email, password) Values ('{$this->name}', '{$this->surname}', '{$this->email}', '{$this->password}')";
+            if($conn->query($sql)){
+                $this->id = $conn->insert_id;// wartosc ostatniego id do bazy danych
+                return true;
+            }else{
+                echo $conn->error;
+                return false;
+            }
+        }else {
+            $sql = "UPDATE User SET  name = '{$this->name}', surname =  '{$this->surname}', email = '{$this->email}', password='{$this->password}' WHERE id = '{$this->id}'";
+        }
+        if($conn->query($sql)){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
